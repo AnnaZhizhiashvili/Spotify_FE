5116555e57f7c45472a6b1f42b0dedf8f0087deb
Ana.Zhizhiashvili <ana.zhiziashvili@protonmail.com>
2024-03-23 22:26:22 +0400

    finish footer, add new colors to tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,7 @@ module.exports = {
         base: '#121212',
         baseGray: '#b3b3b3',
         lightGray: '#727272',
+        mediumGray: '#292929',
         highlight: '#1a1a1a',
         press: '#000',
         elevatedBase: '#242424',
