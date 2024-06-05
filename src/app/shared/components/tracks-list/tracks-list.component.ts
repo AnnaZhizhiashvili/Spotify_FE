28ab503710390930475074067eab42b3bd48d9e3
AnnaZhizhiashvili <ana.zhizhiashvili@protonmail.com>
2024-06-05 21:47:22 +0400

    * update to Angular18 * use signals and computed method

--- src/app/shared/components/tracks-list/tracks-list.component.ts
+++ src/app/shared/components/tracks-list/tracks-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit, Signal } from '@angular/core';
+import { Component, computed, effect, input, Input, OnInit, signal, Signal } from '@angular/core';
 import { TruncatePipe } from '../../pipes/truncate.pipe';
 import { TableModule } from 'primeng/table';
 import { ArrayToStringPipe } from '../../pipes/array-to-string.pipe';
@@ -15,9 +15,16 @@ import { ArrayToStringPipe } from '../../pipes/array-to-string.pipe';
   styleUrl: './tracks-list.component.scss'
 })
 export class TracksListComponent implements OnInit {
-  @Input() tracks: Signal<any>
+  @Input() tracks = signal({tracks: []})
+  customizedTracks = computed(() => this.tracks()?.tracks?.map((track: any) => ({...track, isActive: false })))
   ngOnInit() {
+  }
 
+  selectTrack(id: string) {
+    const selectedTrack = this.customizedTracks().find((track: { id: string; }) => track.id === id)
+    if(selectedTrack) {
+      selectedTrack.isActive =!selectedTrack.isActive;
+    }
   }
 
 }
